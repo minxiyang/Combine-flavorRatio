e2698c85c829c40552bf18a25f440f6d7b5f72d8
minxiyang <[yhzz598@gmail.com]>
2021-12-05 17:53:51 -0500

    update

---
+++ runFrVsCut_Phase2.py
@@ -0,0 +1,111 @@
+from func.tmpHandle import tmpHandle
+from func.writeDatacards import writeDatacards
+from func.getAccEffAndErr import getAccEffAndErr
+from func.runCB import runCB
+from plotting.plotNll import plotNll
+from plotting.plotTmps import plotTmps
+from Parameters import sys_uncers 
+from func.getFrAndLimits import getFrAndLimits
+from plotting.plotFrVsCut import plotFrVsCut
+from func.writeTxt import writeTxt
+
+def main():
+
+    scanRange=(0.4, 2.0)
+    massCuts=range(400, 1201, 100)
+    #massCuts=[1250]
+    frsLeft2={}
+    frsLeft1={}
+    frsMed={}
+    frsRight1={}
+    frsRight2={}
+
+    print("produce flavor ratio as a function of the mass cutoff")
+    for massCut in massCuts:
+        cardNames=[]
+        for year in ["Phase2"]:
+            for cg in ["bb","be"]:
+                if year=="Phase2": scale="Phase2"
+                else: scale="Run2" 
+                key=year+cg
+                cardName="ch"+year+cg+"_"+str(massCut)+"cut"
+                tmpName="tmp"+year+cg+"_"+str(massCut)+"cut"
+                fileName="output"+year+cg+"_"+str(massCut)+"cut"
+                plotName="nll"+year+cg+"_"+str(massCut)+"cut"
+                cardNames.append(cardName)
+                acc_eff = getAccEffAndErr(year, cg, massCut)
+                if year=="Phase2":tmps=tmpHandle("2018", cg, scale=scale)
+                else: tmps=tmpHandle(year, cg, scale=scale)
+                tmps.createTmps(massCut, sys_uncers)
+
+                tmps.saveTmps(tmpName)
+                #plotTmps(year, cg, tmps.templates)
+                
+                writeDatacards(cardName, tmpName, "Run3", cg, tmps.templates, acc_eff)
+                runCB(scanRange, fileName, 0, cardName)
+                fr=getFrAndLimits(fileName, 0)
+                print(fr)
+                inv=0.2
+                #while fr[0]==0. or fr[4]==0.:
+                #    print("rerun combine")
+                #    print((fr[2]-inv, fr[2]+inv)) 
+                #    runCB((fr[2]-inv, fr[2]+inv), fileName, 0, cardName)
+                #    fr=getFrAndLimits(fileName, 0)
+                #    inv=inv*0.9
+                #    print(fr)
+                if key in frsLeft2.keys(): frsLeft2[key].append(fr[0])
+                else: frsLeft2[key]=[fr[0]]
+                if key in frsLeft1.keys(): frsLeft1[key].append(fr[1])
+                else: frsLeft1[key]=[fr[1]]
+                if key in frsMed.keys(): frsMed[key].append(fr[2])
+                else: frsMed[key]=[fr[2]]
+                if key in frsRight1.keys(): frsRight1[key].append(fr[3])
+                else: frsRight1[key]=[fr[3]]
+                if key in frsRight2.keys(): frsRight2[key].append(fr[4])
+                else: frsRight2[key]=[fr[4]]        
+                 
+            
+            
+        runCB(scanRange, "Phase2Combine", 0, *cardNames)
+        fr=getFrAndLimits("Phase2Combine")
+        print(fr)
+        inv=0.2
+        #while fr[0]==0. or fr[4]==0.:
+        #    print("rerun combine")
+        #    print((fr[2]-inv, fr[2]+inv))
+        #    runCB((fr[2]-inv, fr[2]+inv), fileName, 0, *cardNames)
+        #    fr=getFrAndLimits(fileName, 0)
+        #    inv=inv*0.9
+        #    print(fr)
+
+        key="Phase2Combine"
+        if key in frsLeft2.keys(): frsLeft2[key].append(fr[0])
+        else: frsLeft2[key]=[fr[0]]
+        if key in frsLeft1.keys(): frsLeft1[key].append(fr[1])
+        else: frsLeft1[key]=[fr[1]]
+        if key in frsMed.keys(): frsMed[key].append(fr[2])
+        else: frsMed[key]=[fr[2]]  
+        if key in frsRight1.keys(): frsRight1[key].append(fr[3])
+        else: frsRight1[key]=[fr[3]]
+        if key in frsRight2.keys(): frsRight2[key].append(fr[4])
+        else: frsRight2[key]=[fr[4]]
+
+    writeTxt("frByCutL2",frsLeft2)
+    writeTxt("frByCutL1",frsLeft1)
+    writeTxt("frByCutM",frsMed)
+    writeTxt("frByCutR1",frsRight1)
+    writeTxt("frByCutR2",frsRight2)
+    print(frsLeft1)
+    print(frsMed)
+    print(frsRight1)
+    print(frsRight2)
+    for year in ["Phase2"]:
+        for cg in ["bb","be"]:
+            key=year+cg
+            plotFrVsCut(frsLeft2[key], frsLeft1[key], frsMed[key], frsRight1[key], frsRight2[key], massCuts, key)
+
+    key="Phase2Combine"    
+    plotFrVsCut(frsLeft2[key], frsLeft1[key], frsMed[key], frsRight1[key], frsRight2[key], massCuts, "Phase2Combine")    
+
+if __name__=="__main__":
+    main()
--- testPhase2.py
+++ testPhase2.py
@@ -16,32 +16,34 @@ def main():
     scanRange=(0.2, 1.8)
     massCut=400
     print("test run with mass cutoff "+str(massCut))
-    for year in ["2016", "2017", "2018"]:
+    for year in ["Phase2"]:
+        #for cg in ["bb","be"]:
+        if year == "Phase2": 
+            scale="Phase2"
         for cg in ["bb","be"]:
-        
             cardName="ch"+year+cg+"_"+str(massCut)+"cut"
             tmpName="tmp"+year+cg+"_"+str(massCut)+"cut"
             fileName="output"+year+cg+"_"+str(massCut)+"cut"
             plotName="nll"+year+cg+"_"+str(massCut)+"cut"
             cardNames.append(cardName)
             acc_eff = getAccEffAndErr(year, cg, massCut)
-            tmps=tmpHandle(year, cg)
+            tmps=tmpHandle("2018", cg, scale=scale)
             tmps.createTmps(massCut, sys_uncers)
 
             tmps.saveTmps(tmpName)
-            plotTmps(year, cg, massCut, tmps.templates)
-            writeDatacards(cardName, tmpName, year, cg, tmps.templates, acc_eff)
+            plotTmps("Phase2", cg, massCut, tmps.templates)
+            writeDatacards(cardName, tmpName, "Run3", cg, tmps.templates, acc_eff)
             runCB(scanRange, fileName, 0, cardName)
             inputs={"result":fileName}
-            #plotNll(year, cg, massCut, False, plotName, **inputs)
+            plotNll("Phase2", cg, massCut, False, "Phase2", 0, **inputs)
             impactOut="Impact_cut"+str(massCut)+"_"+year+"_"+cg
-            #plotImpact(cardName, impactOut)
-            #plotImpact(cardName, impactOut, False)
+                #plotImpact(cardName, impactOut)
+                #plotImpact(cardName, impactOut, False)
             
             
-    runCB(scanRange, "allYearCombine_cut"+str(massCut), 0, *cardNames)
-    inputs={"result":"allYearCombine_cut"+str(massCut)}
-    #plotNll("All year", "all category", massCut, False, "allYearCombine_cut"+str(massCut), **inputs)
+    runCB(scanRange, "Phase2Combine_cut"+str(massCut), 0, *cardNames)
+    inputs={"result":"Phase2Combine_cut"+str(massCut)}
+    plotNll("Phase2", "all", massCut, False, "Combine",  0, **inputs)
     #plotImpact("combinedCard", "allYearCombine_cut"+str(massCut))
     #plotImpact("combinedCard", "allYearCombine_cut"+str(massCut), False)
         
