12ac9eb2cb37c90ff1964b1c75fb44d06883d2ce
minxiyang <[yhzz598@gmail.com]>
2022-05-12 15:00:07 -0400

    update

--- example/generate.py
+++ example/generate.py
@@ -9,17 +9,31 @@ nsig = np.random.poisson(Nsig, 1)
 bkgs = np.random.exponential(100, nbkg)
 sigs = np.random.normal(50, 5, nsig)
 
-data = ROOT.TH1D("data", "data", 50, 0, 100)
+h_data = ROOT.TH1D("data_obs", "toy data", 50, 0, 100)
+h_bkg = ROOT.TH1D("bkg", "", 50, 0, 100)
+h_sig = ROOT.TH1D("sig", "", 50, 0, 100)
 
+for x in range(1, 99, 2):
+
+    val = 100.*np.exp(-x/100.)
+    h_bkg.SetBinContent((x+1)/2, val)
+    val = (500./(5*np.sqrt(2*np.pi)))*np.exp(-(x-50)**2/50.)
+    h_sig.SetBinContent((x+1)/2, val)
 for x in bkgs:
-    data.Fill(x)
+    h_data.Fill(x)
 
 for x in sigs:
-    data.Fill(x)
+    h_data.Fill(x)
 
+h_bkg.SetLineColor(2)
+h_sig.SetLineColor(3) 
+stack = ROOT.THStack()
+stack.Add(h_bkg)
+stack.Add(h_sig)
 c = ROOT.TCanvas('c', 'c', 800, 800)
-data.SetMarkerStyle(8)
-data.Draw("p")
+h_data.SetMarkerStyle(8)
+h_data.Draw("p")
+stack.Draw("l")
 c.Print("example.pdf")
 
 
